5343e5c17096fe0cc19bcbfe276d779398f4a428
Rubensice <rubensmc10@gmail.com>
2023-09-12 21:55:06 -0300

    filter

--- meuApp/views.py
+++ meuApp/views.py
@@ -3,17 +3,24 @@ from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CharacterAPIView(ModelViewSet):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'location']
+
 
 class LocationAPIView(ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'dimension']
     
 class EpisodeAPIView(ModelViewSet):
     queryset = Episode.objects.all()
     serializer_class = EpisodeSerializer
-    
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'episode']
